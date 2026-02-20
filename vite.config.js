4d896ba870fc44f93b0d18f247c97eec32a5da8a
VikasRajbher <vikasrajbher@gmail.com>
2026-02-20 18:56:32 +0530

    vercel deployment changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Docs./', // Must match your repo name exactly, including case
   plugins: [react()],
 })
